7e8e18642eaf71ce7779fb983d676038def07c0b
Vasyl Khomenko <maroccostar@i.ua>
2024-04-26 16:22:57 +0300

    Added keyboard listener

--- FoodDeliveryApp/FoodDeliveryApp/UILayer/Scenes/AuthScene/Controllers/LoginViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Scenes/AuthScene/Controllers/LoginViewController.swift
@@ -29,6 +29,8 @@ class LoginViewController: UIViewController {
     // MARK: - Properties
     private var state: LoginViewState = .initial
     var viewOutput: LoginViewOutput!
+    private var isKeyboardShow = false
+    private var bottomCTValue = 0.0
     
     // MARK: - Views
     private lazy var bottomView = FDBottomView()
@@ -43,6 +45,9 @@ class LoginViewController: UIViewController {
     private lazy var signInButton = FDButton()
     private lazy var signUpButton = FDButton()
     private lazy var verticalStack = UIStackView()
+    
+    // MARK: - Constraints
+    private var stackViewBottomCT = NSLayoutConstraint()
 
     // MARK: - Initializers
     init(viewOutput: LoginViewOutput, state: LoginViewState) {
@@ -61,6 +66,11 @@ class LoginViewController: UIViewController {
 
         view.backgroundColor = .white
         setupLayout()
+        setupObservers()
+    }
+    
+    deinit {
+        stopKeyboardListener()
     }
     
     func facebookPress() {
@@ -111,17 +121,21 @@ private extension LoginViewController {
         case .signIn:
             verticalStack.addArrangedSubview(signInUserName)
             verticalStack.addArrangedSubview(signInPassword)
+            bottomCTValue = -262
+            stackViewBottomCT = verticalStack.bottomAnchor.constraint(equalTo: bottomView.topAnchor, constant: bottomCTValue)
             NSLayoutConstraint.activate([
                 verticalStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                verticalStack.bottomAnchor.constraint(equalTo: bottomView.topAnchor, constant: -262)
+                stackViewBottomCT
             ])
         case .signUp:
             verticalStack.addArrangedSubview(signUpUserName)
             verticalStack.addArrangedSubview(signUpPassword)
             verticalStack.addArrangedSubview(signUpReEnterPass)
+            bottomCTValue = -227
+            stackViewBottomCT = verticalStack.bottomAnchor.constraint(equalTo: bottomView.topAnchor, constant: bottomCTValue)
             NSLayoutConstraint.activate([
                 verticalStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                verticalStack.bottomAnchor.constraint(equalTo: bottomView.topAnchor, constant: -227)
+                stackViewBottomCT
             ])
         }
     }
@@ -307,6 +321,46 @@ extension LoginViewController: LoginViewInput {
     }
 }
 
+
+// MARK: - Observers
+private extension LoginViewController {
+    func setupObservers() {
+        startKeyboardListener()
+    }
+    func startKeyboardListener() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    func stopKeyboardListener() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        if !isKeyboardShow {
+            UIView.animate(withDuration: 0.3) {
+                self.stackViewBottomCT.constant -= keyboardHeight/4
+                self.view.layoutIfNeeded()
+                self.isKeyboardShow = true
+            }
+        }
+    }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if isKeyboardShow {
+            UIView.animate(withDuration: 0.3) {
+                self.stackViewBottomCT.constant = self.bottomCTValue
+                self.view.layoutIfNeeded()
+                self.isKeyboardShow = false
+            }
+        }
+
+    }
+}
 
 //#Preview("LoginVC") {
 //    LoginViewController(viewOutput: LoginPresenter(), state: .initial)
